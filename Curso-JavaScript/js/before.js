9eaf9e718219a571d9d8f79962d8117c99718a5d
Luis Abdo <luis.abdo2006@gmail.com>
2025-04-01 11:45:01 -0300

    Inicio curso js

---
+++ Curso-JavaScript/js/before.js
@@ -0,0 +1,16 @@
+// 1. Prerequisites
+
+// -> Basic knowledge of JS
+// --->Data types:number, string, boolean, array and object
+// -->operators {=,-,*, /, &&, ||, >=, <=,...}
+// --> If, For, while
+
+//2. Node
+// Node JS
+
+// 3. Visual studio code
+// _> code runner
+// code-runner:clear previous output
+// -> ctrl+alt+n (run code)
+// --ctrl + alt + m (stop the running code)
+console.log('test')
